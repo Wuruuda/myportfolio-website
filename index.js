8da4961f41f714b4a66317f46a4bd214927160e7
Wuruuda <Wardahaanim@gmail.com>
2024-06-28 11:04:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//variable
+const generalBtn=document.getElementById("genetal");
+const businessBtn=document.getElementById("genetal");
+const sportsBtn=document.getElementById("genetal");
+const entertainmantBtn=document.getElementById("genetal");
+const searchBtn=document.getElementById("genetal");
+
+//apis
+
+
+generalBtn.addEventListener("click" ,functionn(){
+});
+
+businessBtn.addEventListener("click" ,functionn(){
+});
+
+sportsBtn.addEventListener("click" ,functionn(){
+});
+
+entertainmentBtn.addEventListener("click" ,functionn(){
+});
+
+searchBtn.addEventListener("click" ,functionn(){
+});
